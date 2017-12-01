e899fed2c9131fe966b8e8b8f569197e26aa294c
Serg.S <sulymkaserg@gmail.com>
2017-12-01 16:01:47 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,54 @@
 $(document).ready(function(){
 	
 	if(window.screen.width>=770) {
-		$('.home-desc-wrap, .news-wrap, .more-links-item, .product-slider .item > div').matchHeight({ property: 'min-height' });
+		$('.home-desc-wrap, .news-wrap, .explor-div, .more-links-item, .product-slider .item > div').matchHeight({ property: 'min-height' });
 			el = '';
+		
 	}
 	
 	$('.filter-btn').click(function(){
 		$(this).toggleClass('active');
 	});
 	
+	// profile edit
+	$('.btn-edit').click(function(){
+		$(this).removeClass('shows');
+		$('.profile-details').addClass('edit-details');
+		$(this).addClass('hide');
+		$('.btn-save, .btn-cancel').removeClass('hide');
+	});
+	$('.btn-save, .btn-cancel').click(function(){
+		$('.profile-details').removeClass('edit-details');
+		$('.btn-edit').removeClass('hide').addClass('shows');
+		$(this).addClass('hide');
+	});
+	// company edit
+	$('.btn-edit-company').click(function(){
+		$(this).removeClass('shows');
+		$('.company-wrap').addClass('edit-company');
+		$(this).addClass('hide');
+		$('.btn-save-company, .btn-cancel-company').removeClass('hide');
+		$('.company-item form.link').removeClass('hide').addClass('shows');
+		$('.company-item .media-link').removeClass('hide').addClass('shows');
+		$('.company-item .social-choice').removeClass('hide').addClass('shows');
+		$('.company-link').removeClass('shows').addClass('hide');
+		
+	});
+	$('.btn-save-company, .btn-cancel-company').click(function(){
+		$('.company-wrap').removeClass('edit-company');
+		$('.btn-edit-company').removeClass('hide').addClass('shows');
+		$(this).addClass('hide');
+		$('.company-item form.link').removeClass('shows').addClass('hide');
+		$('.company-item .media-link').removeClass('shows').addClass('hide');
+		$('.company-item .social-choice').removeClass('shows').addClass('hide');
+		$('.company-link').removeClass('hide').addClass('shows');
+	});
+	$('form input.input-drag').change(function () {
+		$('form span.placeholder').text(this.files.length + " file(s) selected");
+	});
+	
 // Configure/customize these variables.
-	var showChar = 180;  // How many characters are shown by default
+	var showsChar = 180;  // How many characters are showsn by default
 	var ellipsestext = "";
 	var moretext = "[More]";
 	var lesstext = "[Hide]";
@@ -24,10 +62,10 @@ $(document).ready(function(){
 	$('.more').each(function() {
 		var content = $(this).html();
 		
-		if(content.length > showChar) {
+		if(content.length > showsChar) {
 			
-			var c = content.substr(0, showChar);
-			var h = content.substr(showChar, content.length - showChar);
+			var c = content.substr(0, showsChar);
+			var h = content.substr(showsChar, content.length - showsChar);
 			
 			var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="btn-more">' + moretext + '</a></span>';
 			
